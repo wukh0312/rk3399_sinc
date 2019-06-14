aa0f7d4765d68ecc06294e72ca0028144d1dee6e
wukh0312 <wukaihua312@163.com>
2019-06-14 16:33:17 +0800

    add device dir
    source file commit

---
+++ device/rockchip/rk3399/BoardConfig.mk
@@ -0,0 +1,93 @@
+#
+# Copyright 2014 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Use the non-open-source parts, if they're present
+-include vendor/rockchip/rk3399/BoardConfigVendor.mk
+-include device/rockchip/common/BoardConfig.mk
+
+TARGET_BASE_PARAMETER_IMAGE := device/rockchip/common/baseparameter/baseparameter_fb720.img
+
+TARGET_PREBUILT_KERNEL := kernel/arch/arm64/boot/Image
+
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_SMP := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53.a57
+
+
+# Disable emulator for "make dist" until there is a 64-bit qemu kernel
+BUILD_EMULATOR := false
+
+TARGET_BOARD_PLATFORM := rk3399
+TARGET_BOARD_PLATFORM_GPU := mali-t860
+BOARD_USE_DRM := true
+
+# for audio 3A algorithm
+BOARD_USE_AUDIO_3A := false
+
+# RenderScript
+# OVERRIDE_RS_DRIVER := libnvRSDriver.so
+BOARD_OVERRIDE_RS_CPU_VARIANT_32 := cortex-a53
+BOARD_OVERRIDE_RS_CPU_VARIANT_64 := cortex-a53
+# DISABLE_RS_64_BIT_DRIVER := true
+
+TARGET_USES_64_BIT_BCMDHD := true
+TARGET_USES_64_BIT_BINDER := true
+# BOARD_USE_AFBC_LAYER := true
+
+# HACK: Build apps as 64b for volantis_64_only
+ifneq (,$(filter ro.zygote=zygote64, $(PRODUCT_DEFAULT_PROPERTY_OVERRIDES)))
+TARGET_PREFER_32_BIT_APPS :=
+TARGET_SUPPORTS_64_BIT_APPS := true
+endif
+
+# Sensors
+BOARD_SENSOR_ST := false
+BOARD_SENSOR_MPU_PAD := true
+BOARD_SENSOR_MPU_VR := false
+#BOARD_USES_GENERIC_INVENSENSE := false
+
+# GPU MaliT860 support opengl aep
+BOARD_OPENGL_AEP := true
+
+# Copy rk3399 own init.rc file
+# TARGET_PROVIDES_INIT_RC := true
+
+
+//MAX-SIZE=2G, for generate out/.../system.img
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736
+
+
+#rotate screen to 0, 90, 180, 270 for recovery
+#0:   ROTATION_NONE
+#90:  ROTATION_RIGHT
+#180: ROTATION_DOWN
+#270: ROTATION_LEFT
+TARGET_RECOVERY_DEFAULT_ROTATION ?= ROTATION_NONE
+
+ENABLE_CPUSETS := true
+#for optee support
+PRODUCT_HAVE_OPTEE ?= false
+
+BOARD_USE_SPARSE_SYSTEM_IMAGE := true
---
+++ device/rockchip/rk3399/rk3399_all.mk
@@ -0,0 +1,141 @@
+#
+# Copyright 2014 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(LOCAL_PATH)/rk3399.mk)
+
+PRODUCT_NAME := rk3399_all
+PRODUCT_DEVICE := rk3399_all
+PRODUCT_MODEL := rk3399-all
+
+PRODUCT_AAPT_CONFIG := normal large mdpi tvdpi hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+PRODUCT_SYSTEM_VERITY := true
+
+# debug-logs
+ifneq ($(TARGET_BUILD_VARIANT),user)
+MIXIN_DEBUG_LOGS := true
+endif
+
+BOARD_USE_APP_ALARM_ALIGNMENT ?= false
+
+# google apps
+BUILD_WITH_GOOGLE_MARKET := false
+BUILD_WITH_GOOGLE_MARKET_ALL := false
+BUILD_WITH_GOOGLE_FRP := false
+
+#for data encrypt options
+BUILD_WITH_FORCEENCRYPT := true
+
+#for GMS Certification
+BUILD_WITH_GMS_CER := false
+
+#for drm widevine
+BUILD_WITH_WIDEVINE := true
+
+#for cts requirement
+ifeq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.adb.secure=1 \
+    persist.sys.usb.config=mtp
+else
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.adb.secure=0 \
+    persist.sys.usb.config=mtp,adb
+endif
+
+BOOT_SHUTDOWN_ANIMATION_RINGING := false
+
+BOARD_NFC_SUPPORT := false
+BOARD_HAS_GPS := false
+
+#for 3G/4G modem dongle support
+#add time 5-29
+BOARD_HAVE_DONGLE := true
+
+
+BOARD_GRAVITY_SENSOR_SUPPORT := true
+BOARD_COMPASS_SENSOR_SUPPORT := true
+BOARD_GYROSCOPE_SENSOR_SUPPORT := true
+BOARD_PROXIMITY_SENSOR_SUPPORT := false
+BOARD_LIGHT_SENSOR_SUPPORT := true
+BOARD_PRESSURE_SENSOR_SUPPORT := false
+BOARD_TEMPERATURE_SENSOR_SUPPORT := false
+BOARD_USB_HOST_SUPPORT := true
+PRODUCT_HAS_CAMERA := true
+TARGET_ROCKCHIP_PCBATEST := false
+
+#for ddr var support
+BOARD_DDR_VAR_ENABLED := true
+
+PRODUCT_COPY_FILES += \
+   device/rockchip/rk3399/rk3399_all/ddr_config.xml:system/etc/ddr_config.xml \
+   device/rockchip/rk3399/rk3399_all/video_status:system/etc/video_status \
+   device/rockchip/common/resolution_white.xml:/system/usr/share/resolution_white.xml
+
+#for rockchip auto test scripts
+PRODUCT_COPY_FILES += \
+   device/rockchip/rk3399/rockchip_test/ddr/memtester_64bit:/system/bin/memtester \
+   device/rockchip/rk3399/rockchip_test/ddr/stressapptest:/system/bin/stressapptest \
+   device/rockchip/rk3399/rockchip_test/ddr/libstlport.so:/system/lib/libstlport.so \
+   device/rockchip/rk3399/rockchip_test/ddr/ddr_test.sh:/system/bin/ddr_test.sh \
+   device/rockchip/rk3399/rockchip_test/ddr/memtester_test.sh:/system/bin/memtester_test.sh \
+   device/rockchip/rk3399/rockchip_test/ddr/stressapptest_test.sh:/system/bin/stressapptest_test.sh \
+   device/rockchip/rk3399/rockchip_test/dvfs/auto_cpu_freq_test.sh:/system/bin/auto_cpu_freq_test.sh \
+   device/rockchip/rk3399/rockchip_test/dvfs/auto_ddr_freq_test.sh:/system/bin/auto_ddr_freq_test.sh \
+   device/rockchip/rk3399/rockchip_test/dvfs/auto_gpu_freq_test.sh:/system/bin/auto_gpu_freq_test.sh \
+   device/rockchip/rk3399/rockchip_test/dvfs/dvfs_test.sh:/system/bin/dvfs_test.sh \
+   device/rockchip/rk3399/rockchip_test/system_monitor/memory_monitor.sh:/system/bin/memory_monitor.sh \
+   device/rockchip/rk3399/rockchip_test/system_monitor/hardware_monitor.sh:/system/bin/hardware_monitor.sh \
+   device/rockchip/rk3399/rockchip_test/rockchip_test.sh:/system/bin/rockchip_test.sh
+
+
+PRODUCT_PACKAGES += \
+    SoundRecorder
+
+# enable sdcardfs to fix problem which caused by ntfs + fuse
+# and also optimize the filesystem access speed.
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.sdcardfs=true
+
+#PRODUCT_HAVE_OPTEE := true
+
+# multi-screen splicing
+BOARD_MULTISCREEN_SPLICING := false
+BOARD_MULTISCREEN_SPLICING_MODE := 0
+ifeq ($(strip $(BOARD_MULTISCREEN_SPLICING)),true)
+#three 1080p screen splicing(1:1:1)
+PRODUCT_PROPERTY_OVERRIDES += \
+   persist.sys.framebuffer.main=5760x1080@60 \
+   persist.sys.framebuffer.aux=5760x1080@60 \
+   persist.sys.dualModeRatioPri=2 \
+   persist.sys.dualModeRatioAux=1
+endif
+
+# config file for multi-screen splicing
+ifeq ($(strip $(BOARD_MULTISCREEN_SPLICING)),true)
+PRODUCT_PROPERTY_OVERRIDES += \
+   persist.sys.dualModeEnable=1 \
+   sys.hwc.compose_policy=0 \
+   sys.gralloc.disable_afbc=1
+ifeq ($(BOARD_MULTISCREEN_SPLICING_MODE),1)
+PRODUCT_PROPERTY_OVERRIDES += \
+   persist.sys.dualModeTB=1
+else
+PRODUCT_PROPERTY_OVERRIDES += \
+   persist.sys.dualModeTB=0
+endif
+endif
